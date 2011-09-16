54e8fbbcc26f150563995758b12b8141e19575e6
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2011-09-16 16:10:08 +0000

    Fix import
    git-svn-id: http://svn.debux.org/webmotion/webmotion-server/trunk@574 9b541bd0-257f-4b9e-bb95-047589f7bf81

--- src/main/java/org/debux/webmotion/server/WebMotionServer.java
+++ src/main/java/org/debux/webmotion/server/WebMotionServer.java
@@ -26,14 +26,11 @@ package org.debux.webmotion.server;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.io.IOUtils;
 import org.debux.webmotion.server.call.Call;
 import org.debux.webmotion.server.call.HttpContext.ErrorData;
 import org.debux.webmotion.server.mapping.Config;
