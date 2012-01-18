cfa9da7ae117b80a2d614e43bd5ad57cba7fd58d
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-01-18 12:02:06 +0000

    Début de l'implantation de MBean pour la supervision
    git-svn-id: http://svn.debux.org/webmotion/trunk@1055 9b541bd0-257f-4b9e-bb95-047589f7bf81

---
+++ webmotion-server/src/main/java/org/debux/webmotion/server/mbean/WebMotionStats.java
@@ -0,0 +1,119 @@
+/*
+ * #%L
+ * Webmotion server
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 - 2012 Debux
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+package org.debux.webmotion.server.mbean;
+
+import java.util.Collections;
+import java.util.Map;
+import org.debux.webmotion.server.WebMotionUtils.LruCache;
+import org.debux.webmotion.server.call.Call;
+import org.debux.webmotion.server.call.HttpContext;
+
+/**
+ *
+ * @author julien
+ */
+public class WebMotionStats implements WebMotionStatsMXBean {
+
+    protected int sizeLastRequest;
+    protected Map<String, Long> lastRequests;
+    
+    protected long requestCount;
+    protected long requestTime;
+    protected long errorRequestCount;
+    
+    public WebMotionStats() {
+        sizeLastRequest = 100;
+        reset();
+    }
+    
+    public void registerCallTime(Call call, long start) {
+        long end = System.currentTimeMillis();
+        long time = end - start;
+        requestTime += time;
+        
+        requestCount ++;
+        
+        HttpContext context = call.getContext();
+        if (context.isError()) {
+            errorRequestCount ++;
+        }
+        
+        String url = context.getUrl();
+        lastRequests.put(url, time);
+    }
+    
+    @Override
+    public void reset() {
+        requestCount = 0;
+        requestTime = 0;
+        errorRequestCount = 0;
+        resetLastRequests();
+    }
+    
+    @Override
+    public void resetLastRequests() {
+        lastRequests = Collections.synchronizedMap(new LruCache<String, Long>(sizeLastRequest));
+    }
+    
+    @Override
+    public void setSizeLastRequests(int size) {
+        sizeLastRequest = size;
+        resetLastRequests();
+    }
+
+    @Override
+    public int getSizeLastRequests() {
+        return sizeLastRequest;
+    }
+    
+    @Override
+    public long getRequestCount() {
+        return requestCount;
+    }
+
+    @Override
+    public long getRequestTime() {
+        return requestTime;
+    }
+    
+    @Override
+    public long getRequestMeansTime() {
+        if (requestCount == 0) {
+            return 0;
+        }
+        return requestTime / requestCount;
+    }
+
+    @Override
+    public long getErrorRequestCount() {
+        return errorRequestCount;
+    }
+
+    @Override
+    public Map<String, Long> getLastRequests() {
+        return lastRequests;
+    }
+    
+}
---
+++ webmotion-server/src/test/java/org/debux/webmotion/server/mbean/WebmotionStatsTest.java
@@ -0,0 +1,115 @@
+/*
+ * #%L
+ * Webmotion in action
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 Debux
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+package org.debux.webmotion.server.mbean;
+
+import org.debux.webmotion.server.call.Call;
+import org.debux.webmotion.server.call.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+/**
+ * 
+ * @author julien
+ */
+public class WebmotionStatsTest {
+
+    private static final Logger log = LoggerFactory.getLogger(WebmotionStatsTest.class);
+    
+    @Test
+    public void testGetRequestCount() {
+        WebMotionStats stats = new WebMotionStats();
+        
+        stats.registerCallTime(new Call() {
+            @Override
+            public HttpContext getContext() {
+                return new HttpContext() {
+                    @Override
+                    public String getUrl() {
+                        return "/test";
+                    }
+                    @Override
+                    public boolean isError() {
+                        return false;
+                    }
+                };
+            }
+        }, System.currentTimeMillis() - 1000);
+        
+        AssertJUnit.assertEquals(1, stats.getRequestCount());
+    }
+    
+    @Test
+    public void testGetErrorRequestCount() {
+        WebMotionStats stats = new WebMotionStats();
+        
+        stats.registerCallTime(new Call() {
+            @Override
+            public HttpContext getContext() {
+                return new HttpContext() {
+                    @Override
+                    public String getUrl() {
+                        return "/error";
+                    }
+                    @Override
+                    public boolean isError() {
+                        return true;
+                    }
+                };
+            }
+        }, System.currentTimeMillis() - 1000);
+        
+        AssertJUnit.assertEquals(1, stats.getErrorRequestCount());
+    }
+    
+    @Test
+    public void testGetLastRequests() {
+        WebMotionStats stats = new WebMotionStats();
+        
+        for (int index = 0; index < 10; index++) {
+            final String url = "/test" + index;
+            
+            stats.registerCallTime(new Call() {
+                @Override
+                public HttpContext getContext() {
+                    return new HttpContext() {
+                        @Override
+                        public String getUrl() {
+                            return url;
+                        }
+                        @Override
+                        public boolean isError() {
+                            return false;
+                        }
+                    };
+                }
+            }, System.currentTimeMillis() - index * 100);
+        }
+        
+        AssertJUnit.assertEquals(10, stats.getLastRequests().size());
+    }
+    
+}
