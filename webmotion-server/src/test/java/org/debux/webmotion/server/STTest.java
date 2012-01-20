23080035d0f5913efe094d79789b5f34a3e51181
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-01-20 10:13:49 +0000

    Premier essai sur StringTemplate
    git-svn-id: http://svn.debux.org/webmotion/trunk@1073 9b541bd0-257f-4b9e-bb95-047589f7bf81

---
+++ webmotion-server/src/test/java/org/debux/webmotion/server/STTest.java
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * Webmotion server
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 - 2012 Debux
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+package org.debux.webmotion.server;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupString;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+/**
+ * Test on StringTempate.
+ * 
+ * @author julien
+ */
+public class STTest {
+   
+    @Test
+    public void testGroup() {
+        String g = "a(x) ::= <<\n"
+                + "<x>\n"
+                + ">>";
+        
+        STGroup group = new STGroupString(g);
+        ST st = group.getInstanceOf("a");
+        
+        String expected = "foo";
+        st.add("x", expected);
+        
+        String result = st.render();
+        AssertJUnit.assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testHtmlTemplate() {
+        String g = "title(name) ::= <<\n"
+                + "\\<h1\\><name>\\<h1\\>\n"
+                + ">>";
+        
+        STGroup group = new STGroupString(g);
+        ST st = group.getInstanceOf("title");
+        
+        st.add("name", "test");
+        
+        String result = st.render();
+        AssertJUnit.assertEquals("<h1>test<h1>", result);
+    }
+    
+}
