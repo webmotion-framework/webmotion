4c80c241ae7ed49b477d8970da37059e7210f7c5
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-02-11 19:32:19 +0000

    Amélioration : - suppression dépendance non utile - ajout du fichier context.xml pour tomcat - exemple de logger
    git-svn-id: http://svn.debux.org/webmotion/trunk@1154 9b541bd0-257f-4b9e-bb95-047589f7bf81

--- webmotion-archetype/src/main/resources/archetype-resources/src/main/java/Base.java
+++ webmotion-archetype/src/main/resources/archetype-resources/src/main/java/Base.java
@@ -2,9 +2,13 @@ package ${package};
 
 import org.debux.webmotion.server.WebMotionController;
 import org.debux.webmotion.server.render.Render;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Base extends WebMotionController {
 
+    private static final Logger log = LoggerFactory.getLogger(Base.class);
+    
     public Render index() {
         return renderView("index.jsp");
     }
