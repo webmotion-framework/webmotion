8624df30ef1b14f744e5c31d98c31ed05c449605
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-02-11 20:25:53 +0000

    Fix des imports
    git-svn-id: http://svn.debux.org/webmotion/trunk@1156 9b541bd0-257f-4b9e-bb95-047589f7bf81

--- webmotion-server/src/test/java/org/debux/webmotion/server/parser/ANTLRParserTest.java
+++ webmotion-server/src/test/java/org/debux/webmotion/server/parser/ANTLRParserTest.java
@@ -24,7 +24,6 @@
  */
 package org.debux.webmotion.server.parser;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import org.antlr.runtime.RecognitionException;
