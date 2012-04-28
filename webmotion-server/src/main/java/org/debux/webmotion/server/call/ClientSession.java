41eb761a50263b4f102802ef260fe7076309e580
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-04-28 09:59:43 +0000

    refs #185: Mise en place des éléments nécéssaires pour la session manque une bonne partie de la logique
    git-svn-id: http://svn.debux.org/webmotion/trunk@1578 9b541bd0-257f-4b9e-bb95-047589f7bf81

--- webmotion-server/src/main/java/org/debux/webmotion/server/call/ClientSession.java
+++ webmotion-server/src/main/java/org/debux/webmotion/server/call/ClientSession.java
@@ -24,20 +24,210 @@
  */
 package org.debux.webmotion.server.call;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import javax.persistence.Transient;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionContext;
+import org.debux.webmotion.server.call.CookieManger.CookieEntity;
+
 /**
  * @author jruchaud
  */
-public class ClientSession {
+public class ClientSession implements HttpSession {
 
-    protected String id;
+    public static final String SESSION_CONTEXT_COOKIE_NAME = "wm_session_context";
+    public static final String ATTRIBUTES_COOKIE_NAME = "wm_attributes";
     
-    protected long creationTime;
+    protected JsonParser parser = new JsonParser();
+    protected Gson gson = new Gson();
+    protected CookieManger manger;
+        
+    protected HttpContext context;
+    protected ClientSessionContext sessionContext;
+    protected JsonObject attributes;
     
-    protected long lastAccessedTime;
+    public static class ClientSessionContext {
+
+        protected String id;
+        protected long creationTime;
+        protected long lastAccessedTime;
+        protected int maxInactiveInterval;
+        
+        @Transient
+        protected boolean newly;
+
+        public ClientSessionContext() {
+            newly = true;
+            creationTime = System.currentTimeMillis();
+        }
+
+        public long getCreationTime() {
+            return creationTime;
+        }
+
+        public void setCreationTime(long creationTime) {
+            this.creationTime = creationTime;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public long getLastAccessedTime() {
+            return lastAccessedTime;
+        }
+
+        public void setLastAccessedTime(long lastAccessedTime) {
+            this.lastAccessedTime = lastAccessedTime;
+        }
+
+        public int getMaxInactiveInterval() {
+            return maxInactiveInterval;
+        }
+
+        public void setMaxInactiveInterval(int maxInactiveInterval) {
+            this.maxInactiveInterval = maxInactiveInterval;
+        }
+
+        public boolean isNewly() {
+            return newly;
+        }
+
+        public void setNewly(boolean newly) {
+            this.newly = newly;
+        }
+    }
+
+    public ClientSession(HttpContext context, String id) {
+        this.context = context;
+        this.manger = new CookieManger(context, id, true, true);
+        
+        CookieEntity sessionContextCookie = this.manger.get(SESSION_CONTEXT_COOKIE_NAME);
+        this.sessionContext = sessionContextCookie.getValue(ClientSessionContext.class);
+        if (this.sessionContext == null) {
+            this.sessionContext = new ClientSessionContext();
+        }
+        
+        CookieEntity attributesCookie = this.manger.get(ATTRIBUTES_COOKIE_NAME);
+        String value = attributesCookie.getValue();
+        if (value == null) {
+            this.attributes = new JsonObject();
+        } else {
+            this.attributes = this.parser.parse(value).getAsJsonObject();
+        }
+    }
     
-    protected int maxInactiveInterval;
+    @Override
+    public long getCreationTime() {
+        return sessionContext.getCreationTime();
+    }
+
+    @Override
+    public String getId() {
+        return sessionContext.getId();
+    }
+
+    @Override
+    public long getLastAccessedTime() {
+        return sessionContext.getLastAccessedTime();
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return context.getServletContext();
+    }
+
+    @Override
+    public void setMaxInactiveInterval(int interval) {
+        sessionContext.setMaxInactiveInterval(interval);
+    }
+
+    @Override
+    public int getMaxInactiveInterval() {
+        return sessionContext.getMaxInactiveInterval();
+    }
+
+    @Override
+    public Object getAttribute(String name) {
+        // Return only JsonElement
+        return attributes.get(name);
+    }
+
+    @Override
+    public Object getValue(String name) {
+        return getAttribute(name);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        String[] names = getValueNames();
+        List<String> list = Arrays.asList(names);
+        return Collections.enumeration(list);
+    }
+
+    @Override
+    public String[] getValueNames() {
+        Set<Entry<String, JsonElement>> entries = attributes.entrySet();
+        String[] result = new String[entries.size()];
+        int index = 0;
+        
+        for (Entry<String, JsonElement> entry : entries) {
+            String name = entry.getKey();
+            result[index++] = name;
+        }
+        return result;
+    }
+
+    @Override
+    public void setAttribute(String name, Object value) {
+        JsonElement element = gson.toJsonTree(value);
+        attributes.add(name, element);
+    }
+
+    @Override
+    public void putValue(String name, Object value) {
+        setAttribute(name, value);
+    }
+
+    @Override
+    public void removeAttribute(String name) {
+        attributes.remove(name);
+    }
+
+    @Override
+    public void removeValue(String name) {
+        removeAttribute(name);
+    }
+
+    @Override
+    public void invalidate() {
+        attributes = new JsonObject();
+        sessionContext = new ClientSessionContext();
+    }
+
+    @Override
+    public boolean isNew() {
+        return sessionContext.isNewly();
+    }
     
-    
-    
-    
+    @Override
+    public HttpSessionContext getSessionContext() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 }
