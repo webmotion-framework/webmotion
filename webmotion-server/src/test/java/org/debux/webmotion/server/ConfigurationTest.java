7f5fea7a52a24ae8cf96a172c3bbf0f6d36e5781
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-05-04 20:42:44 +0000

    refs #237 : Ajout de tests sur common-configuration
    git-svn-id: http://svn.debux.org/webmotion/trunk@1627 9b541bd0-257f-4b9e-bb95-047589f7bf81

---
+++ webmotion-server/src/test/java/org/debux/webmotion/server/ConfigurationTest.java
@@ -0,0 +1,84 @@
+/*
+ * #%L
+ * WebMotion server
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2011 - 2012 Debux
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+package org.debux.webmotion.server;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.DataConfiguration;
+import org.apache.commons.configuration.MapConfiguration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+/**
+ * Test on common-configuration
+ * 
+ * @author julien
+ */
+public class ConfigurationTest {
+    
+    @Test
+    public void testString() throws ConfigurationException {
+        PropertiesConfiguration configuration = new PropertiesConfiguration();
+        configuration.load(new StringReader("name=value"));
+        AssertJUnit.assertEquals("value", configuration.getString("name"));
+    }
+    
+    @Test
+    public void testComposite() throws ConfigurationException {
+        PropertiesConfiguration first = new PropertiesConfiguration();
+        first.load(new StringReader("name=value"));
+        
+        PropertiesConfiguration second = new PropertiesConfiguration();
+        second.load(new StringReader("name=other"));
+        
+        CompositeConfiguration composite = new CompositeConfiguration();
+        composite.addConfiguration(second);
+        composite.addConfiguration(first);
+        
+        AssertJUnit.assertEquals("other", composite.getString("name"));
+    }
+    
+    @Test
+    public void testData() throws ConfigurationException {
+        PropertiesConfiguration test = new PropertiesConfiguration();
+        test.load(new StringReader("color=#0000FF"));
+        
+        DataConfiguration configuration = new DataConfiguration(test);
+        AssertJUnit.assertNotNull(configuration.getColor("color"));
+    }
+    
+    @Test
+    public void testMap() throws ConfigurationException {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("test", "10");
+        
+        MapConfiguration configuration = new MapConfiguration(map);
+        AssertJUnit.assertEquals(10, configuration.getInt("test"));
+    }
+}
