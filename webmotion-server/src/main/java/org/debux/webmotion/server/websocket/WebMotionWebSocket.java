f21873fb818d4b7a2bb61fb03303cb6b667e9eb5
julien <julien@9b541bd0-257f-4b9e-bb95-047589f7bf81>
2012-09-16 17:43:45 +0000

    refs #235: Create WebSocket structure
    git-svn-id: http://svn.debux.org/webmotion/trunk@2079 9b541bd0-257f-4b9e-bb95-047589f7bf81

---
+++ webmotion-server/src/main/java/org/debux/webmotion/server/websocket/WebMotionWebSocket.java
@@ -0,0 +1,58 @@
+package org.debux.webmotion.server.websocket;
+
+/*
+ * #%L
+ * WebMotion server
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2011 - 2012 Debux
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * Simple class to manage the websocket. The outbound object use to send message to 
+ * the client. Only message text is supported.
+ * 
+ * @author julien
+ */
+public class WebMotionWebSocket implements WebSocketInbound {
+
+    protected WebSocketOutbound outbound;
+    
+    @Override
+    public void setOutbound(WebSocketOutbound outbound) {
+        this.outbound = outbound;
+    }
+
+    @Override
+    public WebSocketOutbound getOutbound() {
+        return outbound;
+    }
+    
+    @Override
+    public void receiveTextMessage(String message) {
+    }
+
+    @Override
+    public void onOpen() {
+    }
+
+    @Override
+    public void onClose() {
+    }
+}
---
+++ webmotion-server/src/main/java/org/debux/webmotion/server/websocket/WebSocketInbound.java
@@ -0,0 +1,60 @@
+package org.debux.webmotion.server.websocket;
+
+/*
+ * #%L
+ * WebMotion server
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2011 - 2012 Debux
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+/**
+ * Interface use to manage inbound connection for a websocket.
+ * 
+ * @author julien
+ */
+public interface WebSocketInbound {
+    
+    /**
+     * Set the outbound to send message for the client.
+     * @param outbound 
+     */
+    void setOutbound(WebSocketOutbound outbound);
+    
+    /**
+     * @return the current outbound
+     */
+    WebSocketOutbound getOutbound();
+    
+    /**
+     * Call when the client send a message.
+     * @param message 
+     */
+    void receiveTextMessage(String message);
+    
+    /**
+     * Call when the client open a new connection.
+     */
+    void onOpen();
+    
+    /**
+     * Call when the client close the current connection.
+     */
+    void onClose();
+}
